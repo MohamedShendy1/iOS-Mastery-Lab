2a2f9fdb14ba366a20cf47509e30987d4c100c39
Mohamed Shendy <dev.mohamed.shendy@gmail.com>
2026-02-25 21:44:18 +0200

    Initial Commit

---
+++ SWiftUI Learning/SWiftUI_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWiftUI_LearningApp.swift
+//  SWiftUI Learning
+//
+//  Created by mac on 25/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SWiftUI_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SWiftUI LearningTests/SWiftUI_LearningTests.swift
@@ -0,0 +1,16 @@
+//
+//  SWiftUI_LearningTests.swift
+//  SWiftUI LearningTests
+//
+//  Created by mac on 25/02/2026.
+//
+
+import Testing
+
+struct SWiftUI_LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
